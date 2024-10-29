a1cfb70c38c806f6e2f9f8e66174035c7ed1e3cc
irfanasyed <132118395+irfanasyed@users.noreply.github.com>
2024-10-29 14:41:29 +0530

    Add files via upload

---
+++ ExampleSerialization/JsonSerializableEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExampleSerialization
+{
+    [Serializable]
+    public class Tutorial
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutorial obj = new Tutorial();
+            obj.ID = 1;
+            obj.Name = ".Net";
+
+            // Serialize to JSON
+            var json = JsonSerializer.Serialize(obj);
+            Console.WriteLine(json);
+
+            // Deserialize from JSON
+            Tutorial deserializedObj = JsonSerializer.Deserialize<Tutorial>(json);
+            Console.WriteLine($"ID: {deserializedObj.ID}, Name: {deserializedObj.Name}");
+        }
+
+    }
+}
---
+++ ExampleSerialization/Tutorial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ExampleSerialization
+{
+    [Serializable]
+
+    internal class Tutorial
+    {
+        public int ID;
+        public string Name;
+        static void Main(string[] args)
+        {
+            Tutorial obj = new Tutorial();
+            obj.ID = 1;
+            obj.Name = ".Net";
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryWriter writer = new BinaryWriter(stream)
+;
+                writer.Write(obj.ID);
+                writer.Write(obj.Name);
+
+                // Get the binary data
+                byte[] binaryData = stream.ToArray();
+
+                // To deserialize:
+                using (MemoryStream readStream = new MemoryStream(binaryData))
+                {
+                    BinaryReader reader = new BinaryReader(readStream);
+                    Tutorial deserializedObj = new Tutorial();
+                    deserializedObj.ID = reader.ReadInt32();
+                    deserializedObj.Name = reader.ReadString();
+                }
+            }
+        }
+    }
+}
